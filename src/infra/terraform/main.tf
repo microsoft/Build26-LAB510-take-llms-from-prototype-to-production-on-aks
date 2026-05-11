8e0f8f2688b3823be2333fce73c1632731ab7b1a
Paul Yu <paul.d.yu@gmail.com>
2026-05-11 12:56:02 -0700

    fix: update default location variable to Poland Central

--- src/infra/terraform/main.tf
+++ src/infra/terraform/main.tf
@@ -55,7 +55,7 @@ provider "kubectl" {
 variable "location" {
   description = "The Azure region to deploy resources in."
   type        = string
-  default     = "Brazil South"
+  default     = "Poland Central"
 }
 
 variable "github_app_id" {
